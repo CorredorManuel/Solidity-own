378c592f55bd1e03e6a7ba5dfc68c1bb48fe7275
"Manuel" <"macamilo2893@gmail.com">
2022-06-22 01:11:04 -0300

    First commit

--- BLOQUE 1 - Conceptos básicos de Solidity/Tema 1 - Primeros pasos/ERC20.sol
+++ BLOQUE 1 - Conceptos básicos de Solidity/Tema 1 - Primeros pasos/ERC20.sol
@@ -4,7 +4,6 @@ pragma experimental ABIEncoderV2;
 import "./SafeMath.sol";
 
 
-
 //Juan Gabriel ---> 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
 //Juan Amengual ---> 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
 //María Santos ---> 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db
