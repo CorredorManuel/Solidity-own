b0d802dd33e882878e0101e7f49678633bd4130b
"Manuel" <"macamilo2893@gmail.com">
2022-06-22 01:03:00 -0300

    second commit

---
+++ BLOQUE 1 - Conceptos básicos de Solidity/Tema 1 - Primeros pasos/Conceptos_Basicos.sol
@@ -0,0 +1,23 @@
+//Indicamos la versión
+pragma solidity>=0.4.4 <0.7.0;
+//Importar el archivo ERC20.sol que esta en nuestro directorio de trabajo
+import "./ERC20.sol";
+
+//Nuestro primer contrato
+contract PrimerContrato{
+
+    //en esta variable se encuentra la dirección de la persona que despliega el contrato
+    address owner;
+    ERC20Basic token;
+
+    /*
+    Guardamos en la variable owner la dirección de la persona que despliega el contrato
+    inicializamos el numero de tokens
+    */
+    constructor() public{
+       owner = msg.sender;
+       token = new ERC20Basic(1000);
+    }
+}
+
+
